2681715fd832316be00e1a67a7f418e8a36002c0
unknown <joel.hacke@gmail.com>
2022-12-08 22:55:06 -0600

    what am i even doing

--- server.tsx
+++ server.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
-import { h, html } from "https://deno.land/x/htm@0.0.10/mod.tsx";
-import { UnoCSS } from "https://deno.land/x/htm@0.0.10/plugins.ts";
+import html, { h } from "https://deno.land/x/htm/mod.ts";
+import UnoCSS from "https://deno.land/x/htm/plugins/unocss.ts";
 
 // enable UnoCSS
 html.use(UnoCSS())
@@ -10,15 +10,18 @@ const joelPoints = 35
 const lianPoints = 24
 
 const handler = (req: Request) => html({
+  classes: {
+    body: ['bg-slate-900']
+  },
   title: "The In Season Cup",
   body: (
     <div
-      class="text-center bg-slate-900 w-full h-screen pt-12"
+      class="text-center w-full bg-slate-900 h-screen pt-2 md:pt-12 p-2"
     >
     <h1 class="text-7xl text-gray-200 mb-6">The In Season Cup</h1>
-    <div class="text-blue-700 flex justify-center gap-12">
+    <div class="text-blue-700 flex flex-col justify-center md:gap-12 md:flex-row">
       <section>
-        <div class="flex justify-center">
+        <div class="flex justify-center -mb-8">
           <img src="https://img.icons8.com/color/96/null/fairytale.png"/>
         </div>
         <h1 class="text-9xl mb-4">
@@ -31,13 +34,15 @@ const handler = (req: Request) => html({
           vs
         </h3>
       </div>
-      <div>
-        <img class="invisible" src="https://img.icons8.com/color/96/null/fairytale.png"/>
+      <section>
+        <div class="hidden justify-center -mb-8 md:flex md:block md:invisible">
+          <img class="" src="https://img.icons8.com/color/96/null/fairytale.png"/>
+        </div>
         <h1 class="text-9xl mb-4">
           Lian
         </h1>
         <p class="text-blue-300 text-6xl">{ lianPoints }</p>
-      </div>
+      </section>
     </div>
   </div>
   ),
